4474d53445a9d6d2cfd400c121a6db900f90e3e2
Raul <rchfe23@gmail.com>
2018-09-26 04:09:36 -0400

    add maps functions

--- genutil.py
+++ genutil.py
@@ -1,15 +1,6 @@
-import ast
-try:
-    # Python 3
-    from configparser import ConfigParser, NoOptionError, NoSectionError
-except ImportError:
-    # Python 2.7
-    from ConfigParser import NoOptionError, NoSectionError
-    from ConfigParser import SafeConfigParser as ConfigParser
 import codecs
 from datetime import datetime
 import json
-import linecache
 import logging.config
 import os
 import pathlib
@@ -17,12 +8,15 @@ import platform
 import pickle
 # TODO: cPickle for Python 2?
 import sqlite3
-import sys
 # Third-party modules
 # TODO: check that if it is right to only load modules from third-party when needed
 import pytz
 import tzlocal
 import yaml
+
+
+def add_plural(v):
+    return "s" if v > 1 else ""
 
 
 def connect_db(db_path, autocommit=False):
@@ -89,36 +83,8 @@ def creation_date(path_to_file):
             return stat.st_mtime
 
 
-# update: existing JSON data will not be overwritten, it will be updated with
-# the new JSON data
-def dump_json(data, path, update=False):
-    def dump_data(data, path):
-        try:
-            with open(path, "w") as f:
-                json.dump(data, f)
-        except FileNotFoundError:
-            print_exception("FileNotFoundError")
-            return 1
-        else:
-            return 0
-    if os.path.isfile(path) and update:
-        retval = load_json(path)
-        if retval is None:
-            return 1
-        else:
-            try:
-                assert type(data) == dict
-                retval.update(data)
-                return dump_data(retval, path)
-            except AssertionError:
-                print_exception("AssertionError: Type of '{}' is not a dict".format(data))
-                return 1
-    else:
-        return dump_data(data, path)
-
-
-def dump_json_with_codecs(filepath, data, encoding='utf8', sort_keys=True,
-                          ensure_ascii=False):
+def dump_json(filepath, data, encoding='utf8', sort_keys=True,
+              ensure_ascii=False):
     try:
         with codecs.open(filepath, 'w', encoding) as f:
             f.write(json.dumps(data, sort_keys=sort_keys,
@@ -140,45 +106,6 @@ def dump_pickle(filepath, data):
             pickle.dump(data, f)
     except FileNotFoundError as e:
         raise FileNotFoundError(e)
-
-
-def filter_data(data, min_threshold, max_threshold):
-    # TODO: every time this function is called, the module is loaded
-    import numpy as np
-    # Sanity check on input thresholds
-    if not (data.max() >= min_threshold >= data.min()):
-        min_threshold = data.min()
-    if not (data.max() >= max_threshold >= data.min()):
-        max_threshold = data.max()
-    first_cond = data >= min_threshold
-    second_cond = data <= max_threshold
-    return np.where(first_cond & second_cond)
-
-
-def get_data_type(val):
-    """
-    Given a string, returns its corresponding data type
-
-    ref.: https://stackoverflow.com/a/10261229
-
-    :param val: string value
-    :return: Data type of string value
-    """
-    try:
-        t = ast.literal_eval(val)
-    except ValueError:
-        return str
-    except SyntaxError:
-        return str
-    else:
-        if type(t) is bool:
-            return bool
-        elif type(t) is int:
-            return int
-        elif type(t) is float:
-            return float
-        else:
-            return str
 
 
 def get_local_time(utc_time=None):
@@ -213,37 +140,23 @@ def get_local_time(utc_time=None):
     return local_time
 
 
-def get_option_value(parser, section, option):
-    value_type = get_data_type(parser.get(section, option))
-    try:
-        if value_type == int:
-            return parser.getint(section, option)
-        elif value_type == float:
-            return parser.getfloat(section, option)
-        elif value_type == bool:
-            return parser.getboolean(section, option)
-        else:
-            return parser.get(section, option)
-    except NoSectionError:
-        print_exception("NoSectionError")
-        return None
-    except NoOptionError:
-        print_exception("NoOptionError")
-        return None
+def init_variable(v, default):
+    if v is None:
+        return default
+    else:
+        return v
 
 
 def load_json(path):
-    data = None
     try:
         with open(path, 'r') as f:
             data = json.load(f)
-    except FileNotFoundError:
-        print_exception("FileNotFoundError")
+    except FileNotFoundError as e:
+        raise FileNotFoundError(e)
     return data
 
 
 def load_json_with_codecs(path, encoding='utf8'):
-    data = None
     try:
         with codecs.open(path, 'r', encoding) as f:
             data = json.load(f)
@@ -274,49 +187,6 @@ def load_yaml(f):
         return yaml.load(f)
     except yaml.YAMLError as e:
         raise yaml.YAMLError(e)
-
-
-def print_exception(error=None):
-    """
-    For a given exception, PRINTS filename, line number, the line itself, and
-    exception description.
-
-    ref.: https://stackoverflow.com/a/20264059
-
-    :return: None
-    """
-    exc_type, exc_obj, tb = sys.exc_info()
-    f = tb.tb_frame
-    lineno = tb.tb_lineno
-    filename = f.f_code.co_filename
-    linecache.checkcache(filename)
-    line = linecache.getline(filename, lineno, f.f_globals)
-    if error is None:
-        err_desc = exc_obj
-    else:
-        err_desc = "{}: {}".format(error, exc_obj)
-    # TODO: find a way to add the error description (e.g. AttributeError) without
-    # having to provide the error description as input to the function
-    print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), err_desc))
-
-
-def read_config(config_path):
-    parser = ConfigParser()
-    found = parser.read(config_path)
-    if config_path not in found:
-        print("ERROR: {} is empty".format(config_path))
-        return None
-    options = {}
-    for section in parser.sections():
-        options.setdefault(section, {})
-        for option in parser.options(section):
-            options[section].setdefault(option, None)
-            value = get_option_value(parser, section, option)
-            if value is None:
-                print("[ERROR] The option '{}' could not be retrieved from {}".format(option, config_path))
-                return None
-            options[section][option] = value
-    return options
 
 
 def read_file(filepath):
--- graphutil.py
+++ graphutil.py
@@ -1,13 +1,27 @@
+from collections import OrderedDict
+# Third-party modules
+import ipdb
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from mpl_toolkits.basemap import Basemap
 import numpy as np
 import plotly
 from plotly.graph_objs import Scatter, Layout
+from scipy.spatial import ConvexHull
+# Own modules
+from .genutil import init_variable, load_json_with_codecs
 
 
-def generate_bar_chart(
-        x, y, xlabel, ylabel, title, grid_which='major',
-        yaxis_major_mutiplelocator=20, yaxis_minor_mutiplelocator=10, fig_width=5, fig_height=5):
+def draw_bar_chart(x,
+                   y,
+                   xlabel,
+                   ylabel,
+                   title,
+                   grid_which='major',
+                   yaxis_major_mutiplelocator=20,
+                   yaxis_minor_mutiplelocator=10,
+                   fig_width=5,
+                   fig_height=5):
     # Sanity check on the input arrays
     assert isinstance(x, type(np.array([]))), \
         "generate_bar_chart(): wrong type on input array 'x'"
@@ -17,7 +31,6 @@ def generate_bar_chart(
         "generate_bar_chart(): wrong shape with 'x' and 'y'"
     assert grid_which in ["minor", "major", "both"], \
         "generate_bar_chart(): wrong value for grid_which='{}'".format(grid_which)
-    # width X height
     plt.figure(figsize=(fig_width, fig_height))
     ax = plt.gca()
     index = np.arange(len(x))
@@ -25,9 +38,11 @@ def generate_bar_chart(
     # Add number-text on top of each bar
     for rect in rects:
         height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width() / 2., 1 * height,
+        ax.text(rect.get_x() + rect.get_width() / 2.,
+                1 * height,
                 '%d' % int(height),
-                ha='center', va='bottom')
+                ha='center',
+                va='bottom')
     plt.xticks(index, x)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
@@ -42,10 +57,16 @@ def generate_bar_chart(
     plt.show()
 
 
-def generate_histogram(
-    data, bins, xlabel, ylabel, title, grid_which='major',
-    xaxis_major_mutiplelocator=10000, xaxis_minor_mutiplelocator=1000,
-    yaxis_major_mutiplelocator=5, yaxis_minor_mutiplelocator=1):
+def draw_histogram(data,
+                   bins,
+                   xlabel,
+                   ylabel,
+                   title,
+                   grid_which='major',
+                   xaxis_major_mutiplelocator=10000,
+                   xaxis_minor_mutiplelocator=1000,
+                   yaxis_major_mutiplelocator=5,
+                   yaxis_minor_mutiplelocator=1):
     # TODO: `bin_width": 10000` not used
     # TODO: `xaxis_minor_mutiplelocator` not used
     # Sanity check on the input array
@@ -70,7 +91,143 @@ def generate_histogram(
     plt.show()
 
 
-def generate_pie_chart(values, labels, title):
+# TODO: check the code and improve it
+# ref.: https://stackoverflow.com/a/42685102
+def draw_us_states_names_on_map(basemap, us_states_filepath):
+    us_states = load_json_with_codecs(us_states_filepath)
+    printed_names = []
+    mi_index = 0
+    wi_index = 0
+    for shapedict, state in zip(basemap.states_info, basemap.states):
+        draw_state_name = True
+        short_name = us_states[(shapedict['NAME'])]
+        if short_name == 'PR':
+            continue
+        if short_name in printed_names and short_name not in ['MI', 'WI']:
+            continue
+        if short_name == 'MI':
+            if mi_index != 3:
+                draw_state_name = False
+            mi_index += 1
+        if short_name == 'WI':
+            if wi_index != 2:
+                draw_state_name = False
+            wi_index += 1
+        # center of polygon
+        x, y = np.array(state).mean(axis=0)
+        hull = ConvexHull(state)
+        hull_points = np.array(state)[hull.vertices]
+        # center of convex hull over the polygon points
+        x, y = hull_points.mean(axis=0)
+        if draw_state_name:
+            # You have to align x,y manually to avoid overlapping for little states
+            plt.text(x + .1, y, short_name, ha="center")
+        printed_names += [short_name, ]
+
+
+# `locations_geo_coords` is a dictionary with keys being the location name and
+# the values are their corresponding `geopy.location.Location` objects
+def draw_usa_map(locations_geo_coords,
+                 shape_filepath,
+                 us_states_filepath,
+                 title, fig_width,
+                 fig_height,
+                 annotate_locations=None,
+                 annotation_cfg=None,
+                 basemap_cfg=None,
+                 map_coords_cfg=None,
+                 draw_parallels=True,
+                 draw_meridians=True):
+
+    def get_markersize(**kwargs):
+        count = kwargs['count']
+        marker_scale = kwargs['marker_scale']
+        return int(np.sqrt(count)) * marker_scale
+
+    # Init variables
+    annotate_locations = init_variable(annotate_locations, [])
+    annotation_cfg = init_variable(annotation_cfg, {})
+    basemap_cfg = init_variable(basemap_cfg, {})
+    map_coords_cfg = init_variable(map_coords_cfg, {})
+
+    plt.figure(figsize=(fig_width, fig_height))
+    plt.title(title)
+    basemap = Basemap(projection=basemap_cfg.get('projection', 'lcc'),
+                      llcrnrlon=basemap_cfg.get('llcrnrlon', -119),
+                      llcrnrlat=basemap_cfg.get('llcrnrlat', 22),
+                      urcrnrlon=basemap_cfg.get('urcrnrlon', -64),
+                      urcrnrlat=basemap_cfg.get('urcrnrlat', 49),
+                      lat_1=basemap_cfg.get('lat_1', 39),
+                      lat_2=basemap_cfg.get('lat_2', 45),
+                      lon_0=basemap_cfg.get('lon_0', -95))
+    basemap.readshapefile(shapefile=shape_filepath,
+                          name="states",
+                          drawbounds=True)
+    draw_us_states_names_on_map(basemap, us_states_filepath)
+    mark_map_coords(basemap, locations_geo_coords, get_markersize,
+                    annotate_locations, annotation_cfg, map_coords_cfg)
+    if draw_meridians:
+        basemap.drawmeridians(np.arange(-120, -40, 20), labels=[0, 0, 0, 1])
+    if draw_parallels:
+        basemap.drawparallels(np.arange(25, 65, 20), labels=[1, 0, 0, 0])
+    plt.show()
+
+
+def draw_world_map(locations_geo_coords,
+                   title,
+                   fig_width,
+                   fig_height,
+                   annotate_locations=None,
+                   annotation_cfg=None,
+                   basemap_cfg=None,
+                   map_coords_cfg=None,
+                   draw_coastlines=True,
+                   draw_countries=True,
+                   draw_map_boundary=True,
+                   draw_meridians=True,
+                   draw_parallels=True,
+                   draw_states=True,
+                   fill_continents=True):
+
+    def get_markersize(**kwargs):
+        return kwargs['marker_scale']
+
+    # Init variables
+    annotate_locations = init_variable(annotate_locations, [])
+    annotation_cfg = init_variable(annotation_cfg, {})
+    basemap_cfg = init_variable(basemap_cfg, {})
+    map_coords_cfg = init_variable(map_coords_cfg, {})
+
+    ipdb.set_trace()
+    plt.figure(figsize=(fig_width, fig_height))
+    plt.title(title)
+    basemap = Basemap(projection=basemap_cfg['projection'],
+                      llcrnrlon=basemap_cfg['llcrnrlon'],
+                      llcrnrlat=basemap_cfg['llcrnrlat'],
+                      urcrnrlon=basemap_cfg['urcrnrlon'],
+                      urcrnrlat=basemap_cfg['urcrnrlat'])
+    mark_map_coords(basemap, locations_geo_coords, get_markersize,
+                    annotate_locations, annotation_cfg, map_coords_cfg)
+    if draw_coastlines:
+        basemap.drawcoastlines()
+    if draw_countries:
+        basemap.drawcountries()
+    if draw_map_boundary:
+        basemap.drawmapboundary()
+    if draw_meridians:
+        # basemap.drawmeridians(np.arange(-120, -40, 20), labels=[0, 0, 0, 1])
+        pass
+    if draw_parallels:
+        # basemap.drawparallels(np.arange(25, 65, 20), labels=[1, 0, 0, 0])
+        pass
+    if draw_states:
+        basemap.drawstates()
+    if fill_continents:
+        basemap.fillcontinents()
+    plt.show()
+
+
+def draw_pie_chart(values, labels, title):
     # Sanity check on the input arrays
     assert isinstance(values, type(np.array([]))), \
         "Wrong type on input array 'values'"
@@ -85,8 +242,8 @@ def generate_pie_chart(values, labels, title):
     plt.show()
 
 
-def generate_scatter_plot(x, y, text, title, mode='markers', hovermode='closest',
-                          yaxis_tickformat='$0.0f'):
+def draw_scatter_plot(x, y, text, title, mode='markers', hovermode='closest',
+                      yaxis_tickformat='$0.0f'):
     # TODO: add labels to axes
     assert isinstance(x, type(np.array([]))), "wrong type on input array 'x'"
     assert isinstance(y, type(np.array([]))), "wrong type on input array 'y'"
@@ -100,3 +257,35 @@ def generate_scatter_plot(x, y, text, title, mode='markers', hovermode='closest'
                          hovermode=hovermode,
                          yaxis={'tickformat': yaxis_tickformat})
     })
+
+
+# `get_markersize` is a function to compute the markersize
+def mark_map_coords(basemap, locations_geo_coords, get_markersize,
+                    annotate_locations=None, annotation_cfg=None,
+                    map_coords_cfg=None):
+    # Init variables
+    annotate_locations = init_variable(annotate_locations, [])
+    annotation_cfg = init_variable(annotation_cfg, {})
+    map_coords_cfg = init_variable(map_coords_cfg, {})
+    for location, data in locations_geo_coords.items():
+        # Transform the location's longitude and latitude to the projection map
+        # coordinates
+        geo_coords = data['geo_coords']
+        x, y = basemap(geo_coords.longitude, geo_coords.latitude)
+        # Plot the map coordinates on the map; the size of the marker is
+        # proportional to the number of occurrences of the location in job posts
+        basemap.plot(
+            x, y,
+            color=map_coords_cfg.get('color', 'Red'),
+            marker=map_coords_cfg.get('marker', 'o'),
+            markersize=get_markersize(
+                count=data['count'],
+                marker_scale=map_coords_cfg.get('marker_scale', 1.5)))
+        # Annotate some locations, e.g. the topk locations with the most job posts
+        if location in annotate_locations:
+            plt.text(x, y, location,
+                     fontsize=annotation_cfg.get('fontsize', 5),
+                     fontweight=annotation_cfg.get('fontweight', 'bold'),
+                     ha=annotation_cfg.get('ha', 'left'),
+                     va=annotation_cfg.get('va', 'bottom'),
+                     color=annotation_cfg.get('color', 'k'))
