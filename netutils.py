0a2b19a0655fbfdd2dfd9b5bd0c80f18865caaf9
Raul <rchfe23@gmail.com>
2019-08-24 18:52:19 -0400

    change order

--- netutils.py
+++ netutils.py
@@ -7,6 +7,13 @@ def get_platform():
     return platform.system().lower()
 
 
+def notify(title, text):
+    # TODO: add ref
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+
+
 def ping_alive(host):
     try:
         arg = "-{}1".format('n' if get_platform() == "windows" else 'c')
@@ -15,10 +22,3 @@ def ping_alive(host):
         print(e)
         return False
     return True
-
-
-def notify(title, text):
-    # TODO: add ref
-    os.system("""
-              osascript -e 'display notification "{}" with title "{}"'
-              """.format(text, title))
