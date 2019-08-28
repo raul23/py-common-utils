e9e394bf12dd41166763fa3d4fa97e5574a19ea2
Raul <rchfe23@gmail.com>
2019-08-28 17:46:39 -0400

    Add SQL sanity check exception

--- exceptions/sql.py
+++ exceptions/sql.py
@@ -7,3 +7,8 @@ Extended summary
 
 class EmptyQueryResultSetError(Exception):
     """Raised when a SQL query returns an empty result set, i.e. no rows."""
+
+
+class SQLSanityCheckError(Exception):
+    """Raised when the sanity check on a SQL query fails: e.g. the query's
+    values are not of `tuple` or wrong number of values in the SQL query."""
