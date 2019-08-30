6ae07b9a7f282cbcc7ba5c2d05bac2b787126ef0
Raul <rchfe23@gmail.com>
2019-08-29 23:18:15 -0400

    add logging to create_db()

--- databases/dbutils.py
+++ databases/dbutils.py
@@ -9,10 +9,11 @@ import sqlite3
 import time
 # Custom modules
 from utilities.exceptions.sql import SQLSanityCheckError
+from utilities.logging.logutils import get_logger
 
 
 def connect_db(db_path, autocommit=False):
-    """Create a database connection to a SQLite database
+    """Open a database connection to a SQLite database
 
     Parameters
     ----------
@@ -24,10 +25,18 @@ def connect_db(db_path, autocommit=False):
     Raises
     ------
     sqlite3.Error
+        Raised if any SQLite-related errors occur, e.g. IntegrityError or
+        OperationalError, since sqlite3.Error is the class for all exceptions
+        of the module. See
 
     Returns
     -------
     sqlite3.Connection object
+        Description
+
+    References
+    ----------
+
 
     """
     try:
@@ -40,29 +49,42 @@ def connect_db(db_path, autocommit=False):
         raise sqlite3.Error(e)
 
 
-def create_db(overwrite, db_filepath, schema_filepath):
+def create_db(overwrite, db_filepath, schema_filepath, logging_cfg=None):
     """
 
     Parameters
     ----------
     overwrite : bool
+        Description
     db_filepath : str
+        Description
     schema_filepath : str
+        Description
+    logging_cfg : dict, optional
+        Description
+
+    Raises
+    ------
+    IOError
+        Raised if
 
     """
     # NOTE: if you need to suppress printing, check the following links
     # - https://bit.ly/2QKRFPX
     # - https://bit.ly/2HmTIa4
+    # Setup logging
+    # TODO: sanity check type of `logging_cfg`
+    logger = get_logger(__name__, __file__, os.getcwd(), logging_cfg)
     db_exists = os.path.exists(db_filepath)
 
     if overwrite and db_exists:
-        print("WARNING: {} will be overwritten ...".format(db_filepath))
+        logger.warning("{} will be overwritten ...".format(db_filepath))
         # Exit program before delay expires or the database is overwritten
         time.sleep(5)
         os.remove(db_filepath)
 
     if not db_exists or overwrite:
-        print("Creating database")
+        logger.info("Creating database {}".format(db_filepath))
         with sqlite3.connect(db_filepath) as conn:
             try:
                 with open(schema_filepath, 'rt') as f:
@@ -71,9 +93,9 @@ def create_db(overwrite, db_filepath, schema_filepath):
             except IOError as e:
                 raise IOError(e)
             else:
-                print("Database created!")
+                logger.info("Database created!")
     else:
-        print("Database '{}' already exists!".format(db_filepath))
+        logger.warning("Database '{}' already exists!".format(db_filepath))
 
 
 def sql_sanity_check(sql, val):
@@ -88,6 +110,12 @@ def sql_sanity_check(sql, val):
 
         Raises
         ------
+        SQLSanityCheckError
+            Raised when the sanity check on a SQL query fails: e.g. the query's
+            values are not of `tuple` or wrong number of values in the SQL
+            query.
+
+            This is a custom exception.
 
         """
         if type(val) is not tuple:
