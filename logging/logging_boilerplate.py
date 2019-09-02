d1d8dbde58148409dfdc62df8f69c7133b39e64e
Raul <rchfe23@gmail.com>
2019-09-01 22:26:29 -0400

    - Add comments - Catch KeyError when logging_options is not found in logging config dict

--- logging/logging_boilerplate.py
+++ logging/logging_boilerplate.py
@@ -5,7 +5,6 @@ Extended summary
 """
 
 import logging
-import os
 # Third-party modules
 import ipdb
 # Custom modules
@@ -20,22 +19,26 @@ class LoggingBoilerplate:
     Parameters
     ----------
     module_name : str
-        Description
+        Name of the module, e.g. ``'utilities.databases.dbutils'``
     module_file : str
-        Description
+        Module's filename, e.g. run_scraper.py
     cwd : str
-        Description
+        Module's current working directory, e.g. .../lyrics-scraper/script
     logging_cfg : str or dict
-        Description
-    use_default_colors : bool
-        Description
-    use_pycharm_colors : bool
-        Description
+        The logger can be setup through a logging configuration file that can
+        be given either as a file path (``str``) or a dictionary (``dict``).
+    use_default_colors : bool, optional
+        Whether to use the default colors when working in a Unix terminal (the
+        default value is False which might imply that no color will be added to
+        the log messages or that the user is working in a PyCharm terminal).
+    use_pycharm_colors : bool, optional
+        Whether to add color to log messages when working in a PyCharm terminal
+        (the default value is False which might imply that no color will be
+        added to the log messages or that the user is working in a Unix
+        terminal).
 
     """
 
-    # `logging_cfg` can either be the path to the logging configuration file
-    # (i.e. a string) or the logging `dict`
     def __init__(self, module_name, module_file, cwd, logging_cfg,
                  use_default_colors=False, use_pycharm_colors=False):
         self.module_name = module_name
@@ -46,31 +49,39 @@ class LoggingBoilerplate:
             self.use_pycharm_colors = use_pycharm_colors
         else:
             # Sanity check on `logging_cfg`
-            assert isinstance(logging_cfg, dict), "`logging_cfg` must be a `dict`"
-            logging_opts = logging_cfg['logging_options']
-            self.use_default_colors = logging_opts['use_default_colors']
-            self.use_pycharm_colors = logging_opts['use_pycharm_colors']
+            assert isinstance(logging_cfg, dict), \
+                "`logging_cfg` must be a `dict`"
+            try:
+                logging_opts = logging_cfg['logging_options']
+                self.use_default_colors = logging_opts['use_default_colors']
+                self.use_pycharm_colors = logging_opts['use_pycharm_colors']
+            except KeyError:
+                raise KeyError(
+                    "[{}] The logging configuration dict was not previously "
+                    "updated with `logging_options`".format(module_name))
         # Get the console and file loggers
         c_logger, f_logger = self._get_loggers()
         # Init the logging wrapper
         self.lw = LoggingWrapper(
-            c_logger, f_logger, self.use_default_colors, self.use_pycharm_colors)
+            c_logger,
+            f_logger,
+            self.use_default_colors,
+            self.use_pycharm_colors)
         if self.lw.use_colors:
             self.lw.info("The log messages will be colored")
         if self.lw.use_pycharm_colors:
             self.lw.info("The colors of the logging messages are those used "
-                         "for the Pycharm Terminal")
-        # Add the console and file loggers to the logging config file if they
-        # are not there yet
+                         "for the PyCharm terminal")
+        # Add the console and file loggers to the logging config ``dict`` if
+        # they are not there yet
         if isinstance(logging_cfg, dict):
             self._add_loggers([c_logger, f_logger], logging_cfg)
         # Setup loggers from YAML logging configuration file or dict
         self.logging_cfg_dict = self._setup_loggers_from_cfg(logging_cfg)
-        if isinstance(logging_cfg, str):
-            # Update the dict `config` with logging options necessary for setting
-            # up the logging mechanism in the other modules,
-            # e.g. `industries_analyzer.py`
-            # NOTE: `logging_cfg_filepath` not used in this module
+        if isinstance(logging_cfg, str) and False:
+            # Update the dict `config` with logging options necessary for
+            # setting up the logging mechanism in the other custom modules
+            # NOTE: `logging_cfg_filepath` is not used in this module
             logging_options = {
                 'logging_options':
                     {
@@ -84,11 +95,13 @@ class LoggingBoilerplate:
     # Add loggers to logging config dict
     @staticmethod
     def _add_loggers(new_loggers, logging_cfg):
-        """
+        """Add loggers to a logging config dictionary.
+
+        TODO: only if the loggers are not already there
 
         Parameters
         ----------
-        new_loggers : list of logger
+        new_loggers : list of logging.Logger
             Description
         logging_cfg : dict
             Description
@@ -102,9 +115,10 @@ class LoggingBoilerplate:
                     logging_cfg['loggers'][new_logger.name] = value
                     break
 
-    # Init the console and file loggers
     def _get_loggers(self):
-        """
+        """Get console and file loggers.
+
+        Init the console and file loggers
 
         Returns
         -------
@@ -122,8 +136,8 @@ class LoggingBoilerplate:
         f_logger = logging.getLogger(
             '{}.f'.format(logger_name))
         # Clear the two loggers from any handlers
-        self.remove_handlers(c_logger)
-        self.remove_handlers(f_logger)
+        self.remove_all_handlers(c_logger)
+        self.remove_all_handlers(f_logger)
         # Setup console logger WITHOUT configuration file
         # IMPORTANT: the file logger will not be setup completely yet, i.e. no
         # handler added. The file logger will be setup later on from the YAML
@@ -135,24 +149,50 @@ class LoggingBoilerplate:
         setup_console_logger(logger=c_logger)
         return c_logger, f_logger
 
-    # `logging_cfg` is the absolute path of the logging configuration file
-    # or a logging ``dict``
     def _setup_loggers_from_cfg(self, logging_cfg):
-        """
+        """Setup console and file loggers from a YAML configuration file or
+        logging ``dict``.
+
+        File and console loggers will be setup through a YAML logging
+        configuration file which defines the loggers, their handlers, and the
+        formatters (how log messages get displayed).
+
+        This logging config file is either given as a file path or a ``dict``.
+
+        Also, a date and time is added to the beginning of the log filename but
+        only in the case that the logging configuration file is given as a file
+        path. Hence, each re-run of the script won't overwrite the previous log
+        file.
 
         Parameters
         ----------
-        logging_cfg
+        logging_cfg : str or dict
+            The YAML configuration file path or the logging ``dict`` that is
+            used to setup the logging.
 
         Returns
         -------
+        logging_cfg_dict : dict
+            The logging configuration ``dict`` that is used to setup the
+            loggers.
+
+        Notes
+        -----
+        The reason why the datetime is only added to the log filename when
+        `logging_cfg` refers to a file path is that this is the case where for
+        the first time the log file is created, i.e. the main script is setting
+        up its logging before any other custom modules.
+
+        Thus afterwards, the other custom modules don't need to bother with the
+        log filename anymore, i.e. no need to add the datetime to the log
+        filename anymore since it has already been done by the main script's
+        logging.
 
         """
-        # TODO: not needed anymore
-        # logging_cfg = os.path.realpath(logging_cfg)
-        # Setup loggers from YAML logging configuration file or logging `dict`
+        # Setup loggers from YAML logging configuration file or logging ``dict``
         try:
             if isinstance(logging_cfg, str):
+                # Case: main script is setting up its logging
                 # Add datetime at the beginning of the log filename
                 # e.g. 2018-09-21-23-24-15-debug.log
                 add_datetime = True
@@ -160,26 +200,41 @@ class LoggingBoilerplate:
                     "Setting up logging from the YAML configuration file "
                     "'{}'".format(logging_cfg))
             else:
+                ipdb.set_trace()
+                # Case: custom modules (other than the main script) are setting
+                # up their logging
                 add_datetime = False
                 self.lw.info(
                     "Setting up logging from a dictionary")
-            logging_cfg_dict = setup_logging_from_cfg(logging_cfg, add_datetime=add_datetime)
+            logging_cfg_dict = setup_logging_from_cfg(logging_cfg,
+                                                      add_datetime=add_datetime)
         except (KeyError, OSError, ValueError) as e:
-            # TODO: write the traceback with one line, see https://bit.ly/2DkH63E
+            # TODO: write the traceback with one line, see
+            # https://bit.ly/2DkH63E
             self.lw.critical(e)
             # TODO: sys.exit(1)?
-            # TODO: raise a custom Exception that could be caught within the script
+            # TODO: raise a custom Exception that could be caught within the
+            # script
             raise SystemExit("Logging could not be setup. Program will exit.")
         else:
             self.lw.debug("Logging setup completed")
             return logging_cfg_dict
 
     def _update_logging_cfg_dict(self, options):
-        """
+        """Update the logging config dictionary with new options.
+
+        It updates the logging config dictionary with logging options necessary
+        for setting up the logging mechanism in other custom modules.
 
         Parameters
         ----------
-        options
+        options :
+
+        Raises
+        ------
+        SystemExit
+            Raised if the new logging options' keys are not unique.
+
 
         """
         self.lw.debug(
@@ -196,21 +251,24 @@ class LoggingBoilerplate:
             self.logging_cfg_dict.update(options)
 
     def get_logger(self):
-        """
+        """Get the logger.
 
         Returns
         -------
+        lw: logging_wrapper.LoggingWrapper
+
 
         """
         return self.lw
 
     @staticmethod
-    def remove_handlers(logger):
-        """
+    def remove_all_handlers(logger):
+        """Remove all handlers from a logger.
 
         Parameters
         ----------
-        logger
+        logger : logging.Logger
+            The logger whose handlers will be removed.
 
         """
         for h in logger.handlers:
