dc620ad9120919e7ba6ab1c671786f4448ada75d
Raul <rchfe23@gmail.com>
2019-09-02 23:25:09 -0400

    - Remove any mention of having two separate loggers for console and file - Disable all non-console loggers when logging in the console

--- logging/logging_wrapper.py
+++ logging/logging_wrapper.py
@@ -1,7 +1,8 @@
-"""Module that defines a logger for logging to a console and file on disk.
+"""Module that defines that allows you to log messages with color.
 
 The module defines a class ``LoggingWrapper`` that creates an API for logging
-to a console and file at the "same time".
+messages with colors where each color represents a log level (by default the
+debug messages are displayed in green).
 
 The log messages can also be colored depending on the type of terminal:
 * standard Unix terminal
@@ -26,14 +27,17 @@ The reason for treating separately the two different types of terminal is that
 the PyCharm terminal might display color levels differently than the standard
 Unix terminal.
 
-For example, the color level '32' will display as yellow on a PyCharm terminal
+For example, the color code '32' will display as yellow on a PyCharm terminal
 and green on a Unix terminal (macOS). You can test it with
 `print("\033[32mtest\033[0m")`.
 
-It's only the log message that can be colored, not the whole log record (like
+It's only the log messages that can be colored, not the whole log record (like
 the module name or the level name).
 
 """
+
+import logging
+
 
 log_levels = ['debug', 'info', 'warning', 'error', 'exception', 'critical']
 
@@ -68,31 +72,28 @@ pycharm_color_levels = {
 
 
 class LoggingWrapper:
-    """Logger that allows you to write to a console and file on disk.
+    """A class that allows you to add color to log messages
 
-    This class creates an API for logging to a console and file at the same
-    time. By calling only one of the pubic logging method (e.g. warning or
-    critical), the message is logged on a console and in a file on disk.
+    This class is a wrapper around ``logging.Logger`` that allows you to add
+    color to your log messages.
+
+    By calling only one of the pubic logging method (e.g. warning and critical),
+    the message can be logged with color, where each color is associated to a
+    log level (e.g. by default the debug messages are displayed in green).
 
     Parameters
     ----------
-    c_logger : logging.Logger
-        A console logger.
-
-    f_logger : logging.Logger
-        A file logger.
-
+    logger : logging.Logger
+        A logger for logging to console and/or file.
     use_default_colors : bool, optional
         Whether to use the default colors when working in a Unix terminal (the
         default value is False which might imply that no color will be added to
         the log messages or that the user is working in a PyCharm terminal).
-
     use_pycharm_colors : bool, optional
         Whether to add color to log messages when working in a PyCharm terminal
         (the default value is False which might imply that no color will be
         added to the log messages or that the user is working in a Unix
         terminal).
-
     color_levels : dict, optional
         The dictionary that defines the colors for the different log level (the
         default value is `default_color_levels` which implies that the colors
@@ -100,13 +101,12 @@ class LoggingWrapper:
 
     """
 
-    # TODO: add option to change `color_levels` from the logging config file.
-    # Right now, we only have two sets of color levels to choose from:
-    # `default_color_levels` and `pycharm_color_levels`
-    def __init__(self, c_logger, f_logger, use_default_colors=False,
+    def __init__(self, logger, use_default_colors=False,
                  use_pycharm_colors=False, color_levels=default_color_levels):
-        self.c_logger = c_logger
-        self.f_logger = f_logger
+        # TODO: add option to change `color_levels` from the logging config file.
+        # Right now, we only have two sets of color levels to choose from:
+        # `default_color_levels` and `pycharm_color_levels`
+        self.logger = logger
         self.use_default_colors = use_default_colors
         self.use_pycharm_colors = use_pycharm_colors
         self.color_levels = color_levels
@@ -118,46 +118,68 @@ class LoggingWrapper:
             self.use_colors = False
 
     def _call_loggers(self, msg, level):
-        """Call the console and file loggers to write a log message.
+        """Call the logger by adding color to the messages.
 
-        This method calls the loggers' logging methods to write a message in a
-        console and file on disk.
+        This method calls the loggers' logging methods to write a message with
+        color (if specified).
 
-        Obviously, only the console logger gets its message colored.
+        Obviously, only the console logger gets its message colored, not the
+        file logger.
 
         Parameters
         ----------
         msg : str or Exception
-            The message (``str``) to be logged in the console and file on disk.
-            The message can also be an ``Exception`` object, e.g.
-            ``TypeError`` or ``sqlite3.IntegrityError``, which will be
-            converted to a string (see _get_error_msg). Obviously, the message
-            should be an ``Exception`` if the log level is 'error', 'exception'
-            or 'critical'.
+            The message (``str``) to be logged. The message can also be an
+            ``Exception`` object, e.g. ``TypeError`` or
+            ``sqlite3.IntegrityError``, which will be converted to a string
+            (see _get_error_msg). Obviously, the message should be an
+            ``Exception`` if the log level is 'error', 'exception' or
+            'critical'.
         level : str
             The name of the log level, e.g. 'debug', 'info'.
 
         """
-        # If `msg` is an Exception, process the ``Exception`` to build the
-        # error message
+        # import ipdb
+        # If `msg` is an ``Exception``, process the ``Exception`` to build the
+        # error message as a string
         msg = self._get_error_msg(msg) if isinstance(msg, Exception) else msg
         # Get the corresponding message-logging functions (e.g. logger.info or
-        # logger.debug) for each of the two loggers
-        c_logger_method = self.c_logger.__getattribute__(level)
-        f_logger_method = self.f_logger.__getattribute__(level)
+        # logger.debug) for the logger
+        logger_method = self.logger.__getattribute__(level)
         # Only the console logger gets colored messages. The file logger don't.
+        # Get the log message with colored code for the console logger
         c_msg = self._set_color(msg, level) if self.use_colors else msg
-        # Call the message-logging functions, e.g. logger.info()
-        c_logger_method(c_msg)
-        f_logger_method(msg)
+        # Disable the other non-console loggers
+        removed_handlers = []
+        if len(self.logger.handlers) > 1:
+            for h in self.logger.handlers:
+                if type(h) is not logging.StreamHandler:
+                    removed_handlers.append(h)
+                    self.logger.removeHandler(h)
+            # Call the message-logging functions, e.g. logger.info()
+            if self.logger.handlers:
+                logger_method(c_msg)
+            for h in removed_handlers:
+                self.logger.addHandler(h)
+            removed_handlers = []
+            for h in self.logger.handlers:
+                if type(h) is logging.StreamHandler:
+                    removed_handlers.append(h)
+                    self.logger.removeHandler(h)
+            if self.logger.handlers:
+                logger_method(msg)
+            for h in removed_handlers:
+                self.logger.addHandler(h)
+        else:
+            # Call the message-logging functions, e.g. logger.info()
+            logger_method(c_msg)
 
     @staticmethod
     def _get_error_msg(exc):
         """Get an error message from an exception.
 
         It converts an error message of type ``Exception`` (e.g.
-        ``sqlite3.IntegrityError`` into a string to be then logged in a console
-        and file.
+        ``sqlite3.IntegrityError`` into a string to be then logged.
 
         Parameters
         ----------
@@ -174,21 +196,24 @@ class LoggingWrapper:
         error_msg = '[{}] {}'.format(exc.__class__.__name__, exc.__str__())
         return error_msg
 
-    # Color the logging message
-    # ref.: https://stackoverflow.com/a/45924203
     def _set_color(self, msg, level):
         """Add color to the log message.
 
         Add color to the log message based on the log level (e.g. by default
-        the debug messages are displayed in green
+        the debug messages are displayed in green).
 
         Parameters
         ----------
         msg : str
-            The message to be logged in a console and a file.
+            The message to be logged.
         level : str
             The name of the log level associated with log message, e.g. 'debug'
             or 'info'.
+
+        References
+        ----------
+        TODO: add link for coloring log messages,https://stackoverflow.com/a/45924203
+
 
         """
         color = self.color_levels[level]
@@ -196,18 +221,20 @@ class LoggingWrapper:
 
     # Logging methods
     def debug(self, msg):
-        """Log a message with the 'debug' log level in a console and a file.
+        """Log a message with the 'debug' log level.
 
         Parameters
         ----------
         msg : str
-            The message to be logged in a console and a file.
+            The message to be logged.
 
         """
         self._call_loggers(msg, 'debug')
 
     def info(self, msg):
-        """Log a message with the 'info' log level in a console and a file.
+        """Log a message with the 'info' log level.
+
+        TODO: message can be colored
 
         Parameters
         ----------
@@ -218,52 +245,52 @@ class LoggingWrapper:
         self._call_loggers(msg, 'info')
 
     def warning(self, msg):
-        """Log a message with the 'warning' log level in a console and a file.
+        """Log a message with the 'warning' log level.
 
         Parameters
         ----------
         msg : str
-            The message to be logged in a console and file.
+            The message to be logged.
 
         """
         self._call_loggers(msg, 'warning')
 
     def error(self, msg):
-        """Log a message with the 'error' log level in a console and file.
+        """Log a message with the 'error' log level.
 
         Parameters
         ----------
         msg : str or Exception
-            The message (``str``) to be logged in the console and file on disk.
-            The message can also be an ``Exception`` object, e.g.
-            ``TypeError`` or ``sqlite3.IntegrityError``, which will be
-            converted to a string (see _get_error_msg).
+             The message (``str``) to be logged. The message can also be an
+            ``Exception`` object, e.g. ``TypeError`` or
+            ``sqlite3.IntegrityError``, which will be converted to a string
+            (see _get_error_msg).
 
         """
         self._call_loggers(msg, 'error')
 
     def exception(self, msg):
-        """Log a message with the 'exception' log level in a console and file.
+        """Log a message with the 'exception' log level.
 
         Parameters
         ----------
         msg : str or Exception
-            The message (``str``) to be logged in the console and file on disk.
-            The message can also be an ``Exception`` object, e.g.
-            ``TypeError`` or ``sqlite3.IntegrityError``, which will be
-            converted to a string (see _get_error_msg).
+             The message (``str``) to be logged. The message can also be an
+            ``Exception`` object, e.g. ``TypeError`` or
+            ``sqlite3.IntegrityError``, which will be converted to a string
+            (see _get_error_msg).
         """
         self._call_loggers(msg, 'exception')
 
     def critical(self, msg):
-        """Log a message with the 'critical' log level in a console and file.
+        """Log a message with the 'critical' log level.
 
         Parameters
         ----------
         msg : str or Exception
-            The message (``str``) to be logged in the console and file on disk.
-            The message can also be an ``Exception`` object, e.g.
-            ``TypeError`` or ``sqlite3.IntegrityError``, which will be
-            converted to a string (see _get_error_msg).
+            The message (``str``) to be logged. The message can also be an
+            ``Exception`` object, e.g. ``TypeError`` or
+            ``sqlite3.IntegrityError``, which will be converted to a string
+            (see _get_error_msg).
         """
         self._call_loggers(msg, 'critical')
