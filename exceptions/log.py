48a08cb1b9d31e6345645100656dc18d3a3bbd2a
Raul <rchfe23@gmail.com>
2019-09-02 23:07:20 -0400

    Add new log-related exception

---
+++ exceptions/log.py
@@ -0,0 +1,9 @@
+"""Module that defines exceptions related to logging problems.
+
+These are the exceptions that are raised when logging.
+
+"""
+
+
+class AddLoggerError(Exception):
+    """Raised when adding a new logger to a logging configuration dictionary."""
