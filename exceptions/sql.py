1e885cff025e4324f0838158b8658e4e7edda982
Raul <rchfe23@gmail.com>
2019-09-04 19:11:42 -0400

    Fix comment

--- exceptions/sql.py
+++ exceptions/sql.py
@@ -6,10 +6,10 @@ These are the exceptions that are raised when querying a SQL database.
 
 
 class EmptyQueryResultSetError(Exception):
-    """Raised when a SQL query returns an empty result set, i.e. no rows."""
+    """Raised if a SQL query returns an empty result set, i.e. no rows."""
 
 
 class SQLSanityCheckError(Exception):
-    """Raised when the sanity check on a SQL query fails: e.g. the query's
+    """Raised if the sanity check on a SQL query fails: e.g. the query's
     values are not of `tuple` type or wrong number of values in the SQL
     query."""
