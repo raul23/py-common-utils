780f6086c389bfdcbe967a3256588c05a998e74c
Raul <rchfe23@gmail.com>
2019-09-04 19:11:15 -0400

    Fix comment

--- exceptions/log.py
+++ exceptions/log.py
@@ -6,4 +6,4 @@ These are the exceptions that are raised when logging.
 
 
 class AddLoggerError(Exception):
-    """Raised when adding a new logger to a logging configuration dictionary."""
+    """Raised if adding a new logger to a logging configuration dictionary."""
