ef62178abeaa2be9a0545876d60e9ae6acad91bc
Raul <rchfe23@gmail.com>
2019-09-04 19:09:41 -0400

    Fix comments

--- exceptions/connection.py
+++ exceptions/connection.py
@@ -6,10 +6,10 @@ These are the exceptions that are raised when querying a server for a resource.
 
 
 class HTTP404Error(Exception):
-    """Raised if the server returns a 404 status code because the page is
+    """Raised if the server returns a 404 status code because the webpage is
     not found."""
     
 
 class WebPageNotFoundError(Exception):
-    """Raised if the webpage HTML could not be retrieved for any reasons,
-    e.g. 404 error, or OSError."""
+    """Raised if the webpage HTML could not be retrieved because of any
+    connection problems, e.g. 404 error."""
