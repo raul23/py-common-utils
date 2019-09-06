2fccd58cb02c8a66dc76d9c507a36b30a50495a3
Raul <rchfe23@gmail.com>
2019-09-05 20:25:55 -0400

    Remove files and folders since they were moved to utils/

--- exceptions/log.py
+++
@@ -1,9 +0,0 @@
-"""Module that defines exceptions related to logging problems.
-
-These are the exceptions that are raised when logging.
-
-"""
-
-
-class AddLoggerError(Exception):
-    """Raised if adding a new logger to a logging configuration dictionary."""
