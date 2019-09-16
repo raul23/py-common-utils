ac67e47bd78541370d49613f8df3a420863b2463
Raul <rchfe23@gmail.com>
2019-09-15 23:33:48 -0400

    Remove WebPageNotFoundError exception

--- utils/exceptions/connection.py
+++ utils/exceptions/connection.py
@@ -8,8 +8,3 @@ These are the exceptions that are raised when querying a server for a resource.
 class HTTP404Error(Exception):
     """Raised if the server returns a 404 status code because the webpage is
     not found."""
-    
-
-class WebPageNotFoundError(Exception):
-    """Raised if the webpage HTML could not be retrieved because of any
-    connection problems, e.g. 404 error."""
