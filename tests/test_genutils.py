4810bc2c4ce8afdbfbf9188a50f5ee180ff5c617
Raul <rchfe23@gmail.com>
2019-10-06 00:44:30 -0400

    Add tests for create_directory()

--- tests/test_genutils.py
+++ tests/test_genutils.py
@@ -8,7 +8,7 @@ import time
 import unittest
 # Custom modules
 from pyutils.genutils import convert_utctime_to_local_tz, create_directory, \
-    run_cmd
+    delete_folder_contents, run_cmd
 import ipdb
 
 
@@ -21,31 +21,46 @@ class TestFunctions(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print("Setting up genutils tests...")
-        # Create temporary directory where all the methods can write
-        cls.tmpdir = TemporaryDirectory()
-        print("Temporary directory created: {}".format(cls.tmpdir.name))
+        # Create main temporary directory
+        cls.main_tmpdir_obj = TemporaryDirectory()
+        cls.main_tmpdir = cls.main_tmpdir_obj.name
+        print("Main temporary directory created: ", cls.main_tmpdir)
+        # Create sandbox directory where the methods can write
+        cls.sanbox_tmpdir = create_directory(
+            os.path.join(cls.main_tmpdir, "sandbox"))
+        print("Sandbox directory created: ", cls.sanbox_tmpdir)
+        # Create a directory for data files (e.g. YAML and txt files) useful
+        # for performing the tests
+        cls.datafiles_tmpdir = create_directory(
+            os.path.join(cls.main_tmpdir, "datafiles"))
+        print("Data files directory created: ", cls.datafiles_tmpdir)
 
     @classmethod
     def tearDown(cls):
-        pass
+        print("Cleanup...")
+        # Cleanup sandbox directory
+        delete_folder_contents(cls.sanbox_tmpdir)
+        # print("Sandbox directory cleared: ", cls.sanbox_tmpdir)
 
     @classmethod
     def tearDownClass(cls):
-        print("\n\nCleanup...")
-        cls.tmpdir.cleanup()
-        print("Temporary directory deleted: {}".format(cls.tmpdir.name))
+        print("\n\nFinal cleanup...")
+        cls.main_tmpdir_obj.cleanup()
+        print("Main temporary directory deleted: ", cls.main_tmpdir_obj.name)
 
     def test_convert_utctime_to_local_tz_case_1(self):
-        """Test case 1 for convert_utctime_to_local_tz()
+        """Test case 1 of convert_utctime_to_local_tz()
 
-        Case 1 consists in setting `utc_time` to a date and time which should be
-        a :obj:`time.struct_time`. It then checks that the returned date and
-        time is equal to the expected one in the local time zone.
+        Case 1 consists in checking that the returned date and time is equal
+        to the expected one in the local time zone.
 
         Notes
         -----
         `utc_time` is the argument given to :meth:`convert_utctime_to_local_tz`
         which will convert it into the local time zone.
+
+        The function sets `utc_time` to a date and time which is a
+        :obj:`time.struct_time`
 
         """
         print("Testing case 1 of convert_utctime_to_local_tz()...")
@@ -60,7 +75,7 @@ class TestFunctions(unittest.TestCase):
         self.assertTrue(output == expected, msg)
 
     def test_convert_utctime_to_local_tz_case_2(self):
-        """Test case 2 for convert_utctime_to_local_tz()
+        """Test case 2 of convert_utctime_to_local_tz()
 
         Case 2 consists in setting `utc_time` to None and checking that the
         returned date and time follows the pattern::
@@ -87,69 +102,106 @@ class TestFunctions(unittest.TestCase):
         regex = r"^\d{4}(-\d{2}){2} (\d{2}:){2}\d{2}-\d{2}:\d{2}$"
         self.assertRegex(output, regex)
 
-    def test_create_directory_dir_exists(self):
-        """Test create_directory() when a directory already exists
+    def test_create_directory_case_1(self):
+        """Test that create_directory() actually created a directory
 
-        Test the case of creating a directory that already exists on disk. The
-        function :meth:`create_directory` should raise a :exc:`FileExistsError`
-        which is an indication that the test passed.
+        Case 1 consists in testing that the directory was actually created on
+        disk.
 
         """
-        print("\nTesting create_directory()...")
-        dirpath = os.path.join(self.tmpdir.name, "testdir")
-        with self.assertRaises(FileExistsError):
+        print("\nTesting case 1 of create_directory()...")
+        dirpath = os.path.join(self.sanbox_tmpdir, "testdir")
+        msg = "The directory {} couldn't be created".format(dirpath)
+        try:
             create_directory(dirpath)
-            create_directory(dirpath)
+        except (FileExistsError, PermissionError) as e:
+            self.fail("{} : {}".format(msg, e))
+        else:
+            self.assertTrue(os.path.isdir(dirpath), msg)
+            print("The directory could be created")
 
-    def test_create_timestamped_dir(self):
+    def test_create_directory_case_2(self):
+        """Test create_directory() when a directory already exists
+
+        Case 2 consists in checking that the function :meth:`create_directory`
+        raises a :exc:`FileExistsError` when we try to create a directory that
+        already exists on disk.
+
+        """
+        print("\nTesting case 2 of create_directory() ...")
+        with self.assertRaises(FileExistsError):
+            dirpath = os.path.join(self.sanbox_tmpdir, "testdir")
+            create_directory(dirpath)
+            create_directory(dirpath)
+        print("Raised a FileExistsError exception as expected")
+
+    def test_create_directory_case_3(self):
+        """Test create_directory() with no permission write in a directory
+
+        Case 3 consists in checking that the function :meth:`create_directory`
+        raises a :exc:`PermissionError` when we try to create a directory in a
+        directory without the write permission.
+
+        """
+        print("\nTesting case 3 of create_directory() ...")
+        testdir1_path = os.path.join(self.sanbox_tmpdir, "testdir1")
+        create_directory(testdir1_path)
+        # TODO: Only works on macOS/Linux:
+        os.chmod(testdir1_path, 0o444)
+        with self.assertRaises(PermissionError):
+            testdir2_path = os.path.join(testdir1_path, "testdir2")
+            create_directory(testdir2_path)
+        print("Raised a PermissionError exception as expected")
+
+    def _test_create_timestamped_dir(self):
         """Test create_timestamped_dir()
 
         """
         print("\nTesting create_timestamped_dir()...")
 
-    def test_dumps_json(self):
+    def _test_dumps_json(self):
         """Test dumps_json()
 
         """
         print("\nTesting dumps_json()...")
 
-    def test_dumps_pickle(self):
+    def _test_dumps_pickle(self):
         """Test dumps_pickle()
 
         """
         print("\nTesting dumps_pickle()...")
 
-    def test_get_creation_date(self):
+    def _test_get_creation_date(self):
         """Test get_creation_date()
 
         """
         print("\nTesting get_creation_date()...")
 
-    def test_load_json(self):
+    def _test_load_json(self):
         """Test load_json()
 
         """
         print("\nTesting load_json()...")
 
-    def test_load_pickle(self):
+    def _test_load_pickle(self):
         """Test load_pickle()
 
         """
         print("\nTesting load_pickle()...")
 
-    def test_load_yaml(self):
+    def _test_load_yaml(self):
         """Test load_yaml()
 
         """
         print("\nTesting load_yaml()...")
 
-    def test_read_file(self):
+    def _test_read_file(self):
         """Test read_file()
 
         """
         print("\nTesting read_file()...")
 
-    def test_read_yaml(self):
+    def _test_read_yaml(self):
         """Test read_yaml()
 
         """
@@ -160,6 +212,7 @@ class TestFunctions(unittest.TestCase):
 
         """
         print("\nTesting run_cmd(cmd='date')...")
+        print("Command output: ")
         self.assertTrue(run_cmd("date") == 0)
 
     def test_run_cmd_pwd(self):
@@ -167,9 +220,10 @@ class TestFunctions(unittest.TestCase):
 
         """
         print("\nTesting run_cmd(cmd='pwd')...")
+        print("Command output: ")
         self.assertTrue(run_cmd("pwd") == 0)
 
-    def test_write_file(self):
+    def _test_write_file(self):
         """Test run_cmd()
 
         """
